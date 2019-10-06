960cf62505d13cfdacf6d7bd4ff8f39b09dee1fc
slowdivePTG <1600012139@pku.edu.cn>
2019-10-06 22:28:22 +0800

    Trace reactions v0.1

--- KROME/AGN_pc/test_stop.f90
+++ KROME/AGN_pc/test_stop.f90
@@ -24,7 +24,7 @@ program test_krome
   xH = 2d4 !Hydrogen density
 
   !user commons for opacity and CR rate
-  call krome_set_user_av(52.99999999999999d0) !opacity Av (#)
+  call krome_set_user_av(5.3d0) !opacity Av (#)
   call krome_set_user_crate(1.3d-17) !CR rate (1/s)
   call krome_set_user_gas_dust_ratio(1d2) !gas/dust
   call krome_init()
@@ -71,7 +71,7 @@ program test_krome
     x1(:)=max(1d-50*xH,x1(:))
     k = k + 1
     t = t + dt !increase time
-    !if (mod(k,10) == 0) call jex(nx,t,x1(:),"./data/Trace_inf")
+    if (mod(k,10) == 0) call trace(nx,t/spy,x1(:),"./data/Trace")
     call krome_set_J21xray(0d0)
     dt = max(dt, (t-1d6*spy)/1d1) !increase time-step
     write(77,'(999E18.8)') t/spy,x1(:)/xH
