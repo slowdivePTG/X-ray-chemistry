9b5c01e3857f517ff7180b474680f20c44a76c52
slowdivePTG <1600012139@pku.edu.cn>
2019-10-17 10:22:39 -0700

    Figures revised

--- KROME/AGN_pc/AGN_plot.py
+++ KROME/AGN_pc/AGN_plot.py
@@ -10,9 +10,10 @@ Col = [
     '#4393c3', '#2166ac'
 ]
 
+
 def latex_name(S):
     if 'DUST' in S:
-        S = S.replace('_DUST','')
+        S = S.replace('_DUST', '')
     Name = {}
     for i in ['H', 'N', 'O', 'C', 'S', 'F', 'M', 'P', 'F']:
         Name[i] = r'\mathrm{' + i + '}'
@@ -27,13 +28,14 @@ def latex_name(S):
         S_n += Name[i]
     return '{}'.format(S_n)
 
+
 class Abu:
     def __init__(self, i):
         txt = './data/2dis' + i
         raw = np.loadtxt(txt)
-        txt_l = './data/1dis' + i #lower limit of extinction
+        txt_l = './data/1dis' + i  #lower limit of extinction
         raw_l = np.loadtxt(txt_l)
-        txt_u = './data/3dis' + i #upper limit of extinction
+        txt_u = './data/3dis' + i  #upper limit of extinction
         raw_u = np.loadtxt(txt_u)
 
         self.t = raw[:, 0]
@@ -49,26 +51,63 @@ class Abu:
         self.abu_u /= raw_u[:, eval('krome_idx_' + div)]
         self.name_div = r'$n{}$'.format(latex_name(div))
 
-    def plot(self, ax, spe='H', label=None, color=None, linestyle='-', Dt=True):
+    def plot(self,
+             ax,
+             spe='H',
+             label=None,
+             color=None,
+             linestyle='-',
+             Dt=True,
+             linewidth=1):
         if Dt:
             t = self.dt
         else:
             t = self.t
-        ax.loglog(t, self.abu[:, eval('krome_idx_' + spe)], label=label, color=color, linestyle=linestyle)
+        ax.loglog(t,
+                  self.abu[:, eval('krome_idx_' + spe)],
+                  label=label,
+                  color=color,
+                  linestyle=linestyle,
+                  linewidth=linewidth)
 
-    def plot_ul(self, ax, spe='H', color=None, Dt=True, Nolabel=True, NoX=False):
+    def plot_u(self, ax, spe='H', color=None, Dt=True, Nolabel=True,
+               NoX=False):
+        linewidth = 3
         if Dt:
             t = self.dt
         else:
             t = self.t
         if Nolabel:
-            label1 = None
-            label2 = None
+            label = None
         else:
-            label1 = r'$N_{\mathrm{H}}=10^{22}$ cm$^{-2}$'
-            label2 = r'$N_{\mathrm{H}}=10^{23}$ cm$^{-2}$'
+            label = r'$N_{\mathrm{H}}=10^{23}$ cm$^{-2}$'
         if NoX:
-            label1 = 'No X-ray, ' + label1
-            label2 = 'No X-ray, ' + label2
-        ax.loglog(t, self.abu_l[:, eval('krome_idx_' + spe)], color=color, linestyle=':', label=label1)
-        ax.loglog(t, self.abu_u[:, eval('krome_idx_' + spe)], color=color, linestyle='-.', label=label2)
+            label = 'No X-ray, ' + label
+            linewidth = 1
+        ax.loglog(t,
+                  self.abu_u[:, eval('krome_idx_' + spe)],
+                  color=color,
+                  linestyle='-.',
+                  label=label,
+                  linewidth=linewidth)
+
+    def plot_l(self, ax, spe='H', color=None, Dt=True, Nolabel=True,
+               NoX=False):
+        linewidth = 3
+        if Dt:
+            t = self.dt
+        else:
+            t = self.t
+        if Nolabel:
+            label = None
+        else:
+            label = r'$N_{\mathrm{H}}=10^{22}$ cm$^{-2}$'
+        if NoX:
+            label = 'No X-ray, ' + label
+            linewidth = 1
+        ax.loglog(t,
+                  self.abu_l[:, eval('krome_idx_' + spe)],
+                  color=color,
+                  linestyle=':',
+                  label=label,
+                  linewidth=linewidth)
